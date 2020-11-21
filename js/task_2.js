b13c2163a61db1443e92de1c4c006f8821366b8a
Daryna <66668459+DarynaProkopchuk@users.noreply.github.com>
2020-11-21 17:02:51 +0400

    task2 done

---
+++ js/task_2.js
@@ -0,0 +1,21 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+const ulList = document.querySelector('#ingredients');
+function createElement(arr) {
+    let itemsArr = [];
+    arr.forEach(item => {
+        const items = document.createElement('li');
+        items.textContent = item;
+        itemsArr.push(items);
+    });
+    return ulList.prepend(...itemsArr);
+}
+createElement(ingredients);
+
+
